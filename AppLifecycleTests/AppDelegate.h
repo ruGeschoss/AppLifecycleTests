09d27c1a88c30d6c11d9784b5924c2586946e53e
Alexander Andrianov <tracktouch@gmail.com>
2021-07-16 17:08:28 +0300

    Initial Commit

---
+++ AppLifecycleTests/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AppLifecycleTests
+//
+//  Created by Alexander Andrianov on 16.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AppLifecycleTests/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  AppLifecycleTests
+//
+//  Created by Alexander Andrianov on 16.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
